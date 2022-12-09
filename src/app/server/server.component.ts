a8cf9cfeb6295714bd9139682ee721cb009e8d3f
gitMarcldo <marcldo@users.noreply.github.com>
2022-12-08 23:20:15 -0500

    server component init

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+//component metadata
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}
